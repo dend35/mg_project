9d1ae51426b6fc594620bc52f3e430e1c118f233
dEND <denis_m@holdingbp.ru>
2020-03-06 10:16:58 +0500

    Сделал лучше

--- MapMerger.Core/MapHelper.cs
+++ MapMerger.Core/MapHelper.cs
@@ -103,8 +103,8 @@ namespace MapMerger.Core
         {
             byte[,][,] mapRotated = new byte[BlocksX, BlocksY][,];
             for (var index0 = 0; index0 < mapRotated.GetLength(0); index0++)
-                for (var index1 = 0; index1 < mapRotated.GetLength(1); index1++) 
-                    mapRotated[index0, index1] = map[index0, index1].RotateMatrixClockwise().ReflectionChunk();
+            for (var index1 = 0; index1 < mapRotated.GetLength(1); index1++) 
+                mapRotated[index0, index1] = map[index0, index1].RotateMatrixClockwise().ReflectionChunk();
 
             return mapRotated;
         }
@@ -223,107 +223,117 @@ namespace MapMerger.Core
 
         #region Colors
 
-        private static readonly Color[] Colors = new Color[255];
+        private static Color[] Colors = new Color[255];
 
         private static void InitColors(MapType type)
         {
-            switch (type)
+            for (int i = 0; i < 255; i++)
             {
-                case MapType.Normal:
-                    Colors[0] = Color.FromRgb(0, 0, 0);
-                    Colors[1] = Color.FromRgb(0, 0, 0);
-                    Colors[32] = Color.FromRgb(0, 0, 0);
-                    Colors[33] = Color.FromRgb(15, 11, 3);
-                    Colors[34] = Color.FromRgb(29, 25, 18);
-                    Colors[35] = Color.FromRgb(68, 68, 68);
-                    Colors[36] = Color.FromRgb(85, 68, 34);
-                    Colors[37] = Color.FromRgb(68, 0, 0);
-                    Colors[38] = Color.FromRgb(51, 68, 0);
-                    Colors[40] = Color.FromRgb(byte.MaxValue, 97, 107);
-                    Colors[41] = Color.FromRgb(byte.MaxValue, 107, 97);
-                    Colors[42] = Color.FromRgb(byte.MaxValue, 107, 107);
-                    Colors[43] = Color.FromRgb(byte.MaxValue, 187, 251);
-                    Colors[44] = Color.FromRgb(191, 241, 251);
-                    Colors[45] = Color.FromRgb(207, 203, 241);
-                    Colors[48] = Color.FromRgb(byte.MaxValue, byte.MaxValue, byte.MaxValue);
-                    Colors[49] = Color.FromRgb(101, 150, 126);
-                    Colors[50] = Color.FromRgb(101, byte.MaxValue, byte.MaxValue);
-                    Colors[51] = Color.FromRgb(byte.MaxValue, 51, 51);
-                    Colors[52] = Color.FromRgb(byte.MaxValue, 101, byte.MaxValue);
-                    Colors[53] = Color.FromRgb(34, 101, byte.MaxValue);
-                    Colors[54] = Color.FromRgb(238, 254, byte.MaxValue);
-                    Colors[55] = Color.FromRgb(238, 254, byte.MaxValue);
-                    Colors[56] = Color.FromRgb(225, 254, byte.MaxValue);
-                    Colors[57] = Color.FromRgb(226, 254, byte.MaxValue);
-                    Colors[58] = Color.FromRgb(227, 254, byte.MaxValue);
-                    Colors[59] = Color.FromRgb(228, 254, byte.MaxValue);
-                    Colors[60] = Color.FromRgb(204, 204, 204);
-                    Colors[61] = Color.FromRgb(221, 221, 221);
-                    Colors[62] = Color.FromRgb(byte.MaxValue, 204, 204);
-                    Colors[63] = Color.FromRgb(byte.MaxValue, 221, 221);
-                    Colors[64] = Color.FromRgb(170, 170, 170);
-                    Colors[65] = Color.FromRgb(187, 187, 187);
-                    Colors[66] = Color.FromRgb(184, 153, 51);
-                    Colors[67] = Color.FromRgb(184, 136, 187);
-                    Colors[68] = Color.FromRgb(119, 68, 68);
-                    Colors[69] = Color.FromRgb(136, 68, 153);
-                    Colors[70] = Color.FromRgb(221, byte.MaxValue, 221);
-                    Colors[71] = Color.FromRgb(71, 215, 100);
-                    Colors[72] = Color.FromRgb(101, 134, 247);
-                    Colors[73] = Color.FromRgb(247, 82, 67);
-                    Colors[74] = Color.FromRgb(132, 238, 247);
-                    Colors[75] = Color.FromRgb(byte.MaxValue, 135, 231);
-                    Colors[82] = Color.FromRgb(17, 102, 102);
-                    Colors[86] = Color.FromRgb(184, byte.MaxValue, 17);
-                    Colors[90] = Color.FromRgb(238, 238, 238);
-                    Colors[91] = Color.FromRgb(byte.MaxValue, 90, 0);
-                    Colors[92] = Color.FromRgb(193, 187, 187);
-                    Colors[93] = Color.FromRgb(187, 193, 187);
-                    Colors[94] = Color.FromRgb(187, 187, 193);
-                    Colors[95] = Color.FromRgb(184, byte.MaxValue, 34);
-                    Colors[96] = Color.FromRgb(184, byte.MaxValue, 68);
-                    Colors[97] = Color.FromRgb(112, 160, 183);
-                    Colors[98] = Color.FromRgb(112, 187, 207);
-                    Colors[99] = Color.FromRgb(219, 209, 125);
-                    Colors[100] = Color.FromRgb(181, 168, 57);
-                    Colors[101] = Color.FromRgb(76, 191, 0);
-                    Colors[102] = Color.FromRgb(208, 206, 0);
-                    Colors[103] = Color.FromRgb(133, 81, 166);
-                    Colors[104] = Color.FromRgb(153, 153, 136);
-                    Colors[105] = Color.FromRgb(198, 0, 0);
-                    Colors[106] = Color.FromRgb(136, 136, 136);
-                    Colors[107] = Color.FromRgb(8, 215, 100);
-                    Colors[108] = Color.FromRgb(byte.MaxValue, 0, 0);
-                    Colors[109] = Color.FromRgb(0, 0, byte.MaxValue);
-                    Colors[110] = Color.FromRgb(byte.MaxValue, 0, byte.MaxValue);
-                    Colors[111] = Color.FromRgb(238, 238, byte.MaxValue);
-                    Colors[112] = Color.FromRgb(0, byte.MaxValue, byte.MaxValue);
-                    Colors[113] = Color.FromRgb(211, 159, 166);
-                    Colors[114] = Color.FromRgb(119, 119, 119);
-                    Colors[115] = Color.FromRgb(56, 118, 65);
-                    Colors[116] = Color.FromRgb(17, 17, byte.MaxValue);
-                    Colors[117] = Color.FromRgb(170, 119, 119);
-                    Colors[118] = Color.FromRgb(100, 98, 21);
-                    Colors[119] = Color.FromRgb(170, byte.MaxValue, byte.MaxValue);
-                    Colors[120] = Color.FromRgb(227, 191, 120);
-                    Colors[121] = Color.FromRgb(163, 136, 72);
-                    Colors[122] = Color.FromRgb(51, 153, 120);
-                    break;
-                case MapType.Alive:
-                    Colors[50] = Color.FromRgb(101, (int)byte.MaxValue, (int)byte.MaxValue);
-                    Colors[51] = Color.FromRgb((int)byte.MaxValue, 51, 51);
-                    Colors[52] = Color.FromRgb((int)byte.MaxValue, 101, (int)byte.MaxValue);
-                    Colors[53] = Color.FromRgb((int)byte.MaxValue, 138, (int)byte.MaxValue);
-                    Colors[54] = Color.FromRgb(238, 254, (int)byte.MaxValue);
-                    Colors[55] = Color.FromRgb(238, 254, (int)byte.MaxValue);
-                    Colors[116] = Color.FromRgb(161, 162, (int)byte.MaxValue);
-                    Colors[119] = Color.FromRgb(170, (int)byte.MaxValue, (int)byte.MaxValue);
-                    break;
+                Colors[i] = Color.Black;
+                
             }
-            
+            Colors[0] = Color.FromRgba(0, 0, 0,255);
+            Colors[1] = Color.FromRgba(0, 0, 0,255);
+            Colors[32] = Color.FromRgba(0, 0, 0,255);
+            Colors[33] = Color.FromRgba(15, 11, 3, 255);
+            Colors[34] = Color.FromRgba(29, 25, 18, 255);
+            Colors[35] = Color.FromRgba(68, 68, 68, 255);
+            Colors[36] = Color.FromRgba(85, 68, 34, 255);
+            Colors[37] = Color.FromRgba(68, 0, 0, 255);
+            Colors[38] = Color.FromRgba(51, 68, 0, 255);
+            Colors[40] = Color.FromRgba(byte.MaxValue, 97, 107, 255);
+            Colors[41] = Color.FromRgba(byte.MaxValue, 107, 97, 255);
+            Colors[42] = Color.FromRgba(byte.MaxValue, 107, 107, 255);
+            Colors[43] = Color.FromRgba(byte.MaxValue, 187, 251, 255);
+            Colors[44] = Color.FromRgba(191, 241, 251, 255);
+            Colors[45] = Color.FromRgba(207, 203, 241, 255);
+            Colors[48] = Color.FromRgba(byte.MaxValue, byte.MaxValue, byte.MaxValue, 255);
+            Colors[49] = Color.FromRgba(101, 150, 126, 255);
+            Colors[50] = Color.FromRgba(101, byte.MaxValue, byte.MaxValue, 255);
+            Colors[51] = Color.FromRgba(byte.MaxValue, 51, 51, 255);
+            Colors[52] = Color.FromRgba(byte.MaxValue, 101, byte.MaxValue, 255);
+            Colors[53] = Color.FromRgba(34, 101, byte.MaxValue, 255);
+            Colors[54] = Color.FromRgba(238, 254, byte.MaxValue, 255);
+            Colors[55] = Color.FromRgba(238, 254, byte.MaxValue, 255);
+            Colors[56] = Color.FromRgba(225, 254, byte.MaxValue, 255);
+            Colors[57] = Color.FromRgba(226, 254, byte.MaxValue, 255);
+            Colors[58] = Color.FromRgba(227, 254, byte.MaxValue, 255);
+            Colors[59] = Color.FromRgba(228, 254, byte.MaxValue, 255);
+            Colors[60] = Color.FromRgba(204, 204, 204, 255);
+            Colors[61] = Color.FromRgba(221, 221, 221, 255);
+            Colors[62] = Color.FromRgba(byte.MaxValue, 204, 204, 255);
+            Colors[63] = Color.FromRgba(byte.MaxValue, 221, 221, 255);
+            Colors[64] = Color.FromRgba(170, 170, 170, 255);
+            Colors[65] = Color.FromRgba(187, 187, 187, 255);
+            Colors[66] = Color.FromRgba(184, 153, 51, 255);
+            Colors[67] = Color.FromRgba(184, 136, 187, 255);
+            Colors[68] = Color.FromRgba(119, 68, 68, 255);
+            Colors[69] = Color.FromRgba(136, 68, 153, 255);
+            Colors[70] = Color.FromRgba(221, byte.MaxValue, 221, 255);
+            Colors[71] = Color.FromRgba(71, 215, 100, 255);
+            Colors[72] = Color.FromRgba(101, 134, 247, 255);
+            Colors[73] = Color.FromRgba(247, 82, 67, 255);
+            Colors[74] = Color.FromRgba(132, 238, 247, 255);
+            Colors[75] = Color.FromRgba(byte.MaxValue, 135, 231, 255);
+            Colors[82] = Color.FromRgba(17, 102, 102, 255);
+            Colors[86] = Color.FromRgba(184, byte.MaxValue, 17, 255);
+            Colors[90] = Color.FromRgba(238, 238, 238, 255);
+            Colors[91] = Color.FromRgba(byte.MaxValue, 90, 0, 255);
+            Colors[92] = Color.FromRgba(193, 187, 187, 255);
+            Colors[93] = Color.FromRgba(187, 193, 187, 255);
+            Colors[94] = Color.FromRgba(187, 187, 193, 255);
+            Colors[95] = Color.FromRgba(184, byte.MaxValue, 34, 255);
+            Colors[96] = Color.FromRgba(184, byte.MaxValue, 68, 255);
+            Colors[97] = Color.FromRgba(112, 160, 183, 255);
+            Colors[98] = Color.FromRgba(112, 187, 207, 255);
+            Colors[99] = Color.FromRgba(219, 209, 125, 255);
+            Colors[100] = Color.FromRgba(181, 168, 57, 255);
+            Colors[101] = Color.FromRgba(76, 191, 0, 255);
+            Colors[102] = Color.FromRgba(208, 206, 0, 255);
+            Colors[103] = Color.FromRgba(133, 81, 166, 255);
+            Colors[104] = Color.FromRgba(153, 153, 136, 255);
+            Colors[105] = Color.FromRgba(198, 0, 0, 255);
+            Colors[106] = Color.FromRgba(136, 136, 136, 255);
+            Colors[107] = Color.FromRgba(8, 215, 100, 255);
+            Colors[108] = Color.FromRgba(byte.MaxValue, 0, 0, 255);
+            Colors[109] = Color.FromRgba(0, 0, byte.MaxValue, 255);
+            Colors[110] = Color.FromRgba(byte.MaxValue, 0, byte.MaxValue, 255);
+            Colors[111] = Color.FromRgba(238, 238, byte.MaxValue, 255);
+            Colors[112] = Color.FromRgba(0, byte.MaxValue, byte.MaxValue, 255);
+            Colors[113] = Color.FromRgba(211, 159, 166, 255);
+            Colors[114] = Color.FromRgba(119, 119, 119, 255);
+            Colors[115] = Color.FromRgba(56, 118, 65, 255);
+            Colors[116] = Color.FromRgba(17, 17, byte.MaxValue,255);
+            Colors[117] = Color.FromRgba(170, 119, 119, 255);
+            Colors[118] = Color.FromRgba(100, 98, 21, 255);
+            Colors[119] = Color.FromRgba(170, byte.MaxValue, byte.MaxValue, 255);
+            Colors[120] = Color.FromRgba(227, 191, 120, 255);
+            Colors[121] = Color.FromRgba(163, 136, 72, 255);
+            Colors[122] = Color.FromRgba(51, 153, 120, 255);
+            if (type == MapType.Alive)
+            {
+                for (var i = 0; i < Colors.Length; i++)
+                {
+                    Colors[i] = Colors[i].WithAlpha(0.2f);
+                }
+
+                Colors[50] = Color.FromRgba(101, (int)byte.MaxValue, (int)byte.MaxValue, 255);
+                Colors[51] = Color.FromRgba((int)byte.MaxValue, 51, 51, 255);
+                Colors[52] = Color.FromRgba((int)byte.MaxValue, 101, (int)byte.MaxValue, 255);
+                Colors[53] = Color.FromRgba((int)byte.MaxValue, 138, (int)byte.MaxValue, 255);
+                Colors[54] = Color.FromRgba(238, 254, (int)byte.MaxValue, 255);
+                Colors[55] = Color.FromRgba(238, 254, (int)byte.MaxValue, 255);
+                Colors[116] = Color.FromRgba(161, 162, (int)byte.MaxValue, 255);
+                Colors[119] = Color.FromRgba(170, (int)byte.MaxValue, (int)byte.MaxValue, 255);
+            }
+                    
+                    
         }
+
+
 
         #endregion
         #endregion
     }
 }
+
